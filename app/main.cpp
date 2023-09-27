643d5935260283893d86f23541823a95240c7dc6
transistasis <46616290+transistasis@users.noreply.github.com>
2023-09-26 21:55:19 -0400

    Removed submoduled JSON library, used FetchContent to import CMake-based libraries

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,9 @@
 #include "my_library.h"
 #include "config.h"
+#include <cxxopts.hpp>
+#include <fmt/format.h>
 #include "nlohmann/json.hpp"
+#include <spdlog/spdlog.h>
 
 int main() {
   std::cout << project_name << " " << project_version << "\n\n";
@@ -9,6 +12,15 @@ int main() {
             << NLOHMANN_JSON_VERSION_MAJOR << "."
             << NLOHMANN_JSON_VERSION_MINOR << "."
             << NLOHMANN_JSON_VERSION_PATCH << "\n";
+
+  std::cout << "FMT:" << FMT_VERSION << "\n";
+
+  std::cout << "CXXOPTS:" << CXXOPTS__VERSION_MAJOR << "."
+            << CXXOPTS__VERSION_MINOR << "." << CXXOPTS__VERSION_PATCH
+            << "\n";
+
+  std::cout << "SPDLOG:" << SPDLOG_VER_MAJOR << "." << SPDLOG_VER_MINOR << "."
+            << SPDLOG_VER_PATCH << "\n";
 
   print_string();
 
